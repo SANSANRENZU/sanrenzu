f2f611a70adb93ce6545a1457aab4d82211b2e23
何松 <2472301489@qq.com>
2016-12-01 14:55:36 +0800

    第二次

--- text/text/ViewController.m
+++ text/text/ViewController.m
@@ -16,6 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSLog(@"234567890-");
+    
+    
+    
+    
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
